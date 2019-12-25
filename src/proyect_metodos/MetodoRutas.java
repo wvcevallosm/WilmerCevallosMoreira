5a35e1db8468bd22401a5b3bb116d8d5debc8870
usuario <usuario@Laptop-Wilmer>
2019-12-25 13:58:20 -0500

    Se agregó un nuevo método para validar que exista el archivo de almacenamiento, caso contrario el usuario debe seleccionar un archivo existente en su disco en la carpeta que se designe.

--- src/proyect_metodos/MetodoRutas.java
+++ src/proyect_metodos/MetodoRutas.java
@@ -1,5 +1,6 @@
 package proyect_metodos;
 
+import java.io.File;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -8,15 +9,42 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 import java.util.Vector;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 import proyect_clases.Boleto;
 import proyect_clases.Rutas;
 
 public class MetodoRutas {
-    
+    String RutaCorrecta="";
     Vector vPrincipal = new Vector();
     
+    /*  Se agregó este método para controlar que el archivo de almacenamiento exista        */
+    /*  caso contrario lo selecciona y guarda su respectiva ruta para poder ser utilizada   */
+    /*  mas adelante en otros procesos                                                      */
+    public String SeleccionaArchivoCorrecto(){
+        File archivo= new File("C:\\Rutas.txt");
+        if (!archivo.exists()){
+            JOptionPane.showMessageDialog(null, "Archivo no existe, favor seleccionar");
+            JFileChooser seleccionaFile = new JFileChooser();
+            seleccionaFile.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            seleccionaFile.setMultiSelectionEnabled(false);
+            FileNameExtensionFilter filtro= new FileNameExtensionFilter("Archivos textos","txt");
+            seleccionaFile.setFileFilter(filtro);
+            
+            int resultado=seleccionaFile.showOpenDialog(null);
+            if (resultado==JFileChooser.APPROVE_OPTION) {
+                RutaCorrecta=seleccionaFile.getSelectedFile().getAbsolutePath();
+             }else{
+                RutaCorrecta="";
+            }
+        }else{
+            RutaCorrecta="C:\\Rutas.txt";
+        }
+        return RutaCorrecta;
+    }//Fin de método seleccionar archivo correcto
+  
     //guarda datos en el vector
     public void guardarRutas(Rutas unaRuta) {
         vPrincipal.addElement(unaRuta);
@@ -24,7 +52,8 @@ public class MetodoRutas {
     //guardar archivo txt
     public void guardarArchivoRutas(Rutas rutas){
         try {
-            FileWriter fw = new FileWriter ("C:\\Rutas.txt", true);
+            RutaCorrecta=SeleccionaArchivoCorrecto();   
+            FileWriter fw = new FileWriter (RutaCorrecta, true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter pw = new PrintWriter(bw);
             pw.print(rutas.getId_Ruta());
@@ -41,6 +70,7 @@ public class MetodoRutas {
     }
     //mostrar los datos en el jtable
     public DefaultTableModel listaRutas(){
+        RutaCorrecta=SeleccionaArchivoCorrecto();
         Vector cabeceras = new Vector();
         cabeceras.addElement("ID");
         cabeceras.addElement("RUTA");
@@ -51,27 +81,32 @@ public class MetodoRutas {
         cabeceras.addElement("FECHA");
         //Crear vector con nombre apellido pasajero cedula edad
         DefaultTableModel mdlTablaR = new DefaultTableModel(cabeceras,0);
-        try {
-            FileReader fr = new FileReader("C:\\Rutas.txt");
-            BufferedReader br = new BufferedReader(fr);
-            String d;
-            while ((d=br.readLine())!=null){
-                StringTokenizer dato = new StringTokenizer (d,"|");
-                Vector x = new Vector();
-                while (dato.hasMoreTokens()){
-                    x.addElement(dato.nextToken());
-                }
-                mdlTablaR.addRow(x);
-            }
-        }catch (Exception e){
-        JOptionPane.showMessageDialog(null, e);
+        if (RutaCorrecta!="") {          
+           try {
+               FileReader fr = new FileReader(RutaCorrecta);
+               BufferedReader br = new BufferedReader(fr);
+               String d;
+               while ((d=br.readLine())!=null){
+                   StringTokenizer dato = new StringTokenizer (d,"|");
+                   Vector x = new Vector();
+                   while (dato.hasMoreTokens()){
+                       x.addElement(dato.nextToken());
+                   }
+                   mdlTablaR.addRow(x);
+               }
+           }catch (Exception e){
+           JOptionPane.showMessageDialog(null, e);
+           }  
+        }else{
+            JOptionPane.showMessageDialog(null, "Selección de Archivo incorrecto");
         }
         return mdlTablaR;
     }
    
     public Vector BuscarRuta(String unaRuta){
         try {
-            FileReader fr = new FileReader("C:\\Rutas.txt");
+            RutaCorrecta=SeleccionaArchivoCorrecto();           
+            FileReader fr = new FileReader(RutaCorrecta);
             BufferedReader br = new BufferedReader(fr);
             String d;
             while ((d=br.readLine())!=null){
